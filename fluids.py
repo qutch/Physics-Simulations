f64a59143e7caa42d7f0b65cd34db11538838855
Hutch Turner <102335368+qutch@users.noreply.github.com>
2024-05-26 12:47:39 -0400

    Implemented collision detection btwn particles

--- fluids.py
+++ fluids.py
@@ -16,6 +16,11 @@ dampeningValue = 0.9
 
 class Particle:
     def __init__(self, pos, vel, acc, radius):
+        """
+        1. Add mass to this simulation
+        2. Follow proper physics formulas for collisons and stuff
+        3. Try adding forming of liquids or something like slime
+        """
         self.pos = np.asarray(pos, dtype=np.float64)
         self.vel = np.asarray(vel, dtype=np.float64)
         self.acc = np.asarray(acc, dtype=np.float64)
@@ -29,16 +34,39 @@ class Particle:
         pass
 
     def update(self):
-        self.accelerate()
+        particle.calcWallCollision()
         self.pos += self.vel
     
     def calcWallCollision(self):
-        if self.pos[1] + self.radius > screen_height:
+        if self.pos[1] + self.radius > screen_height: # Floor Collision
             self.pos[1] = screen_height - self.radius
             self.vel[1] *= -1 * dampeningValue
-        if self.pos[1] - self.radius < 0:
+        if self.pos[1] - self.radius < 0: # Ceiling Collision
             self.pos[1] = self.radius
             self.vel[1] *= -1 * dampeningValue
+        if self.pos[0] + self.radius > screen_width: # Right Wall Collision
+            self.pos[0] = screen_width - self.radius
+            self.vel[0] *= -1 * 0.8
+        if self.pos[0] - self.radius < 0: # Left Wall Collision
+            self.pos[0] = self.radius
+            self.vel[0] *= -1 * 0.8
+    
+    def calcParticleCollision(self, particles):
+        for other in particles:
+            if other != self:
+                distance = np.linalg.norm(other.pos - self.pos)
+                minDistance = self.radius + other.radius
+
+                if distance < minDistance:
+                    overlap = float(self.radius + other.radius - distance)
+                    normal = other.pos - self.pos
+
+                    self.pos += overlap
+                    other.pos -= overlap
+                    
+                    """NEED TO ACTUALLY IMPLEMENT THIS COLLISION!!!"""
+                    other.vel *= -1 
+            else: pass
 
 
 running = True
@@ -53,17 +81,16 @@ while running:
         if event.type == pygame.QUIT:
             running = False
         if event.type == pygame.MOUSEBUTTONDOWN:
-            particle = Particle((mouse_x, mouse_y), [r.randint(-5, 5),r.randint(-5, 5)], [0,0], 10)
+            particle = Particle((mouse_x, mouse_y), [r.choice([-2, -1, 1, 2]), r.choice([-2, -1, 1, 2])], [0,0], 10)
             particles.append(particle)
     
     screen.fill((33, 33, 33))
 
     for particle in particles:
-        particle.draw()
-        particle.calcWallCollision()
+        particle.calcParticleCollision(particles)
         particle.update()
+        particle.draw()
 
-    
     
     pygame.display.update()
     clock.tick(60)
